60384282fc4a46c74e88b9efb6f1b5e57da97f23
DESKTOP-31L1003\Owner <sweetchild00008@gmail.com>
2022-12-08 14:51:35 +0800

    re layout water module

--- resources/views/admin/water.blade.php
+++ resources/views/admin/water.blade.php
@@ -2,57 +2,83 @@
 
 
 @section('content')
-    <div class="card">
-        <div class="card-body">
-            <div class="card-title">
-                <h4>Tank Configurations</h4>
-            </div>
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="form-group row">
-                        <label class="col-sm-3 col-form-label">Tank Height</label>
-                        <div class="col-sm-6">
-                            <input disabled type="text" class="form-control form-control-sm">
-                        </div>
-                        <div class="col-sm-3">
-                            <button class="btn btn-primary" onclick="settank()">Set Tank Height</button>
-                        </div>
+    <div class="row">
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-body">
+                    <div class="card-title">
+                        <h4>Tank Configurations</h4>
                     </div>
-                </div>
-            </div>
-
-            <div class="card-description">Tank Criticals</div>
-            <form action="{{ route('water.update') }}" method="post">
-                @method('put')
-                @csrf
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="form-group row">
-                            <label class="col-sm-3 col-form-label">Dispenser Critical Level (%)</label>
-                            <div class="col-sm-6">
-                                <input type="number" name="dispensertankcritical" class="form-control form-control-sm" max="100" min="1"
-                                    step="1" value="{{$conf->dispensertankcritical??0}}">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="form-group row">
+                                <label class="col-sm-3 col-form-label">Main Tank Height</label>
+                                <div class="col-sm-9">
+                                    <input disabled type="text" class="form-control form-control-sm" value="{{$conf->maintankheight??0}}">
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="form-group row">
-                            <label class="col-sm-3 col-form-label">Main Tank Critical Level (%)</label>
-                            <div class="col-sm-6">
-                                <input type="number" name="maintankcritical" class="form-control form-control-sm" max="100" min="1"
-                                    step="1" value="{{$conf->maintankcritical??0}}">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="form-group row">
+                                <label class="col-sm-3 col-form-label">Dispenser Tank Height</label>
+                                <div class="col-sm-9">
+                                    <input disabled type="text" class="form-control form-control-sm" value={{$conf->dispensertankheight??0}}>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-9 text-right">
-                        <button type="submit" class="btn btn-success">Save</button>
+                    <div class="row">
+                        <div class="offset-md-3 col-md-9">
+                            <button type="button" class="btn btn-primary" onclick="settank()">Set Tank Height</button>
+                        </div>
                     </div>
                 </div>
-            </form>
+            </div>
+        </div>
+        <div class="col-md-6">
+            
+            <div class="card">
+                <div class="card-body">
+                    <div class="card-title">
+                        <h4>Tank Criticals</h4>
+                    </div>
+                    <form action="{{ route('water.update') }}" method="post">
+                        @method('put')
+                        @csrf
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="form-group row">
+                                    <label class="col-sm-3 col-form-label">Dispenser Critical Level (%)</label>
+                                    <div class="col-sm-9">
+                                        <input type="number" name="dispensertankcritical"
+                                            class="form-control form-control-sm" max="100" min="1"
+                                            step="1" value="{{ $conf->dispensertankcritical ?? 0 }}">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="form-group row">
+                                    <label class="col-sm-3 col-form-label">Main Tank Critical Level (%)</label>
+                                    <div class="col-sm-9">
+                                        <input type="number" name="maintankcritical" class="form-control form-control-sm"
+                                            max="100" min="1" step="1"
+                                            value="{{ $conf->maintankcritical ?? 0 }}">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="offset-md-3 col-md-9"">
+                                <button type="submit" class="btn btn-success">Save</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
