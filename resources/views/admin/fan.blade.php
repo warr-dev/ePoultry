9d286de9e54165d6a8f4d542cf8145f40447c43f
DESKTOP-31L1003\Owner <sweetchild00008@gmail.com>
2022-12-18 14:53:39 +0800

    relayout fan

--- resources/views/admin/fan.blade.php
+++ resources/views/admin/fan.blade.php
@@ -10,7 +10,7 @@
 
     <div class="row">
         
-        <div class="col-lg-6 grid-margin stretch-card">
+        <div class="col-lg-12 grid-margin stretch-card">
             <div class="card">
                 <div class="card-body">
                     <h4 class="card-title">Configuration</h4>
