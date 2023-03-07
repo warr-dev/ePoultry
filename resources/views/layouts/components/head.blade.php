d0b0f8bab07bed46495d9d67500f16d814e9d270
Warren Dalawampu <sweetchild00008@gmail.com>
2023-03-08 00:23:03 +0800

    print tables

--- resources/views/layouts/components/head.blade.php
+++ resources/views/layouts/components/head.blade.php
@@ -17,7 +17,8 @@
     <link rel="stylesheet" href="/css/vertical-layout-light/style.css">
     <!-- endinject -->
     <link rel="shortcut icon" href="/images/kulungan.svg" />
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.2/css/jquery.dataTables.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.3/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/2.3.5/css/buttons.dataTables.min.css">
     
   <link rel="stylesheet" href="/vendors/mdi/css/materialdesignicons.min.css">
     @stack('head')
--- resources/views/layouts/components/scripts.blade.php
+++ resources/views/layouts/components/scripts.blade.php
@@ -20,12 +20,24 @@
   <script src="/js/dashboard.js"></script>
   <script src="/js/Chart.roundedBarCharts.js"></script>
   <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.13.2/js/jquery.dataTables.js"></script>
+  <script src="https://cdn.datatables.net/buttons/2.3.5/js/dataTables.buttons.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+  <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/buttons/2.3.5/js/buttons.html5.min.js"></script>
+  <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/buttons/2.3.5/js/buttons.print.min.js"></script>
+  
   
   <!-- End custom js for this page-->
 
   <script>
     $(document).ready( function () {
-    $('.dtt').DataTable();
+    $('.dtt').DataTable({
+        dom: 'Bfrtip',
+        buttons: [
+            'copy', 'csv', 'excel', 'pdf', 'print'
+        ]
+    });
   } );
 
     setInterval(() => {
